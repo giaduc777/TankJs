ffe0976b22fdff570c980bcc63c0a6f911264e14
Duc Le <giaduc777@yahoo.com>
2020-08-20 20:28:46 -0700

    add importLoader: 1

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,14 @@ module.exports = {
             {
                 test: /\.css$/,
                 use:[
-                    'style-loader', 'css-loader', 'postcss-loader'
+                    'style-loader', 
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            importLoaders: 1
+                        }
+                    }, 
+                    'postcss-loader'
                 ]
             }
         ]
